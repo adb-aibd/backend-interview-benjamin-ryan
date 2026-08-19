0fbff2f17154fb6f8d59c185e4c01620efa73ad1
Benjamin Ryan Mendoza <brb.mendoza@gmail.com>
2026-08-19 18:50:29 +0800

    Implement get_rate
    This commit moves the rate date from the path to a query param to make
    it optional
    
    test

--- app/services/exchange_rate_service.py
+++ app/services/exchange_rate_service.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.db.models.currency import Currency
@@ -18,6 +19,50 @@ def _normalize_calculated_value(val: Decimal):
     for consistency with the DB.
     """
     return val.quantize(Decimal("0.0000000001"))
+
+
+def get_exchange_rate(
+    session: Session,
+    rate_date: datetime,
+    base_currency: Currency,
+    quote_currency: Currency,
+    side: ExchangeRateSide,
+) -> ExchangeRate | None:
+    canonical_base, canonical_quote = sorted(
+        (base_currency, quote_currency), key=lambda c: c.iso_code
+    )
+
+    is_flipped = canonical_base.iso_code != base_currency.iso_code
+    canonical_side = (
+        side
+        if not is_flipped
+        else ExchangeRateSide.BUY
+        if side == ExchangeRateSide.SELL
+        else ExchangeRateSide.SELL
+    )
+    # Get latest
+    sql = (
+        select(ExchangeRate)
+        .where(ExchangeRate.base_currency_id == canonical_base.id)
+        .where(ExchangeRate.quote_currency_id == canonical_quote.id)
+        .where(ExchangeRate.rate_date < rate_date + timedelta(days=1))
+        .where(ExchangeRate.side == canonical_side)
+        .order_by(ExchangeRate.rate_date.desc())
+        .limit(1)
+    )
+
+    result = session.scalars(sql).first()
+
+    if is_flipped and result is not None:
+        return ExchangeRate(
+            base_currency=base_currency,
+            quote_currency=quote_currency,
+            side=side,
+            rate=_normalize_calculated_value(1 / result.rate),
+            rate_date=rate_date,
+        )
+
+    return result
 
 
 def post_exchange_rate(
--- openapi_server/controllers/rates_controller.py
+++ openapi_server/controllers/rates_controller.py
@@ -1,4 +1,5 @@
 import flask
+from datetime import date, datetime
 from decimal import Decimal
 from typing import Dict
 from typing import Tuple
@@ -9,27 +10,28 @@ from openapi_server.models.daily_rate import DailyRate  # noqa: E501
 from openapi_server.models.daily_rate_update import DailyRateUpdate  # noqa: E501
 from openapi_server.models.daily_rate_upsert import DailyRateUpsert  # noqa: E501
 from openapi_server.models.error import Error  # noqa: E501
+from openapi_server.models.exchange_rate import ExchangeRate
 from openapi_server.models.transaction_side import TransactionSide  # noqa: E501
 from openapi_server import util
 
 from app.db import SessionFactory
 from app.db.models.exchange_rate import ExchangeRateSide
-from app.services.exchange_rate_service import post_exchange_rate
+from app.services.exchange_rate_service import get_exchange_rate, post_exchange_rate
 
 
-def delete_rate(rate_date, base_currency, quote_currency, side):  # noqa: E501
+def delete_rate(base_currency, quote_currency, side, rate_date=None):  # noqa: E501
     """Delete a daily exchange rate
 
      # noqa: E501
 
-    :param rate_date:
-    :type rate_date: str
     :param base_currency:
     :type base_currency: str
     :param quote_currency:
     :type quote_currency: str
     :param side:
     :type side: dict | bytes
+    :param rate_date:
+    :type rate_date: str
 
     :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
     """
@@ -39,75 +41,125 @@ def delete_rate(rate_date, base_currency, quote_currency, side):  # noqa: E501
     return None, 501
 
 
-def get_rate(rate_date, base_currency, quote_currency, side):  # noqa: E501
+def get_rate(base_currency, quote_currency, side, rate_date=None):  # noqa: E501
     """Get a daily exchange rate
 
      # noqa: E501
 
-    :param rate_date: 
-    :type rate_date: str
-    :param base_currency: 
+    :param base_currency:
     :type base_currency: str
-    :param quote_currency: 
+    :param quote_currency:
     :type quote_currency: str
-    :param side: 
+    :param side:
     :type side: dict | bytes
+    :param rate_date: Filters the rate by the specified date. When specified, the latest rate from that date is returned. If there is no posted rate for the specified date, returns the latest rate.  Must be less than or equal to today&#39;s date (UTC), and greater than UNIX epoch (i.e. 1970-01-01)
+    :type rate_date: str
 
     :rtype: Union[DailyRate, Tuple[DailyRate, int], Tuple[DailyRate, int, Dict[str, str]]
     """
-    rate_date = util.deserialize_date(rate_date)
-    if flask.request.is_json:
-        side =  TransactionSide.from_dict(flask.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    # TODO: Generalize date validation
+    now = datetime.now()
+    if rate_date is None:
+        rate_date = now
+
+    try:
+        validated_rate_date = datetime.strptime(rate_date, "%Y-%m-%d")
+    except ValueError:
+        return {"code": "INVALID_DATE", "message": "rateDate is not a valid date"}, 400
+
+    if validated_rate_date.date() > now.date():
+        return {"code": "INVALID_DATE", "message": "rateDate is in the future"}, 400
+
+    if base_currency == quote_currency:
+        return {
+            "code": "SAME_CURRENCY",
+            "message": "Base and quote currencies cannot be the same.",
+        }, 400
+
+    with SessionFactory() as session, session.begin():
+        currencies = get_currencies_by_codes(
+            session=session,
+            currency_codes={
+                base_currency,
+                quote_currency,
+            },
+        )
+
+        exchange_rate = get_exchange_rate(
+            session=session,
+            rate_date=validated_rate_date,
+            base_currency=currencies[base_currency],
+            quote_currency=currencies[quote_currency],
+            side=ExchangeRateSide(side),
+        )
+
+        return (
+            (
+                DailyRate(
+                    rate_date=exchange_rate.rate_date.date(),
+                    rate=exchange_rate.rate,
+                    base_currency=exchange_rate.base_currency.iso_code,
+                    quote_currency=exchange_rate.quote_currency.iso_code,
+                    side=exchange_rate.side.value,
+                ),
+                200,
+            )
+            if exchange_rate is not None
+            else (
+                {
+                    "code": "DAILY_RATE_NOT_FOUND",
+                    "message": "Daily exchange rate not found.",
+                },
+                404,
+            )
+        )
 
 
-def list_rates(rate_date=None, base_currency=None, quote_currency=None, side=None):  # noqa: E501
+def list_rates(rate_date=None, base_currency=None, quote_currency=None):  # noqa: E501
     """List daily exchange rates
 
-     # noqa: E501
+    Returns latest exchange rates.  If neither base nor quote currencies are specified, returns the latest exchange rates for all supported pairs.  # noqa: E501
 
-    :param rate_date: 
+    :param rate_date: Filters exchange rates by the given date.  If not specified, filters exchange rates by the current date.
     :type rate_date: str
-    :param base_currency: 
+    :param base_currency: Filters exchange rates by the given currency.  If not specified, returns the latest exchange rates for all pairs with the specified quote currency.
     :type base_currency: str
-    :param quote_currency: 
+    :param quote_currency: Filters exchange rates by the given currency.  If not specified, returns the latest exchange rates for all pairs with the specified base currency.
     :type quote_currency: str
-    :param side: 
-    :type side: dict | bytes
 
     :rtype: Union[List[DailyRate], Tuple[List[DailyRate], int], Tuple[List[DailyRate], int, Dict[str, str]]
     """
     rate_date = util.deserialize_date(rate_date)
     if flask.request.is_json:
-        side =  TransactionSide.from_dict(flask.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        side = TransactionSide.from_dict(flask.request.get_json())  # noqa: E501
+    return "do some magic!"
 
 
-def update_rate(rate_date, base_currency, quote_currency, side, body):  # noqa: E501
+def update_rate(base_currency, quote_currency, side, body, rate_date=None):  # noqa: E501
     """Replace a daily exchange rate
 
      # noqa: E501
 
-    :param rate_date: 
-    :type rate_date: str
-    :param base_currency: 
+    :param base_currency:
     :type base_currency: str
-    :param quote_currency: 
+    :param quote_currency:
     :type quote_currency: str
-    :param side: 
+    :param side:
     :type side: dict | bytes
-    :param daily_rate_update: 
+    :param daily_rate_update:
     :type daily_rate_update: dict | bytes
+    :param rate_date:
+    :type rate_date: str
 
     :rtype: Union[DailyRate, Tuple[DailyRate, int], Tuple[DailyRate, int, Dict[str, str]]
     """
     rate_date = util.deserialize_date(rate_date)
     if flask.request.is_json:
-        side =  TransactionSide.from_dict(flask.request.get_json())  # noqa: E501
+        side = TransactionSide.from_dict(flask.request.get_json())  # noqa: E501
     daily_rate_update = body
     if flask.request.is_json:
         daily_rate_update = DailyRateUpdate.from_dict(flask.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return "do some magic!"
 
 
 def post_rate(body):  # noqa: E501
